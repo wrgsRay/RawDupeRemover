c36c6e93b552c338ac849f14f1866ffe55de1ab9
Raymond Wang <28991499+wrgsRay@users.noreply.github.com>
2019-06-30 22:48:56 -0700

    listed all files in the folder, but doesn't do anything as of now

---
+++ main.py
@@ -0,0 +1,19 @@
+"""
+Python 3.6
+@Author: wrgsRay
+"""
+import os
+
+# TODO: Ask user for path
+path_base = 'D:\\2018-11-13'
+
+
+def main():
+    files = [f for f in os.listdir(path_base) if os.path.isfile(os.path.join(path_base, f))]  # Get all files
+    for f in files:
+        # TODO: do something
+        print(f)
+
+
+if __name__ == '__main__':
+    main()
